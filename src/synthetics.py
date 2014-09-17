29a9d7bf8c93b863d1256b7bb1fc9088aae89bb2
Nadege Langet <nadege.langet@etu.unistra.fr>
2014-09-17 11:33:04 +0200

    Introduced varaibility between the test set and the training set (proportions and values ranges)

--- src/synthetics.py
+++ src/synthetics.py
@@ -7,16 +7,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from plot_2features import plot_2f_synthetics, plot_2f_synth_var
 
-def create_synthetics(npts,sig_x,sig_y,theta,mean):
+def create_synthetics(npts,tup):
   """
   Crée un jeu de données synthétiques
   Distributions gaussiennes à 2 variables
   npts : nombre d'échantillons
   sig_x : variance en x
   sig_y : variance en y
+    ==> extension
   theta : inclinaison (rotation) en radians
-  mean : moyenne
+  mean : moyenne (position)
   """
+  sig_x = tup[0]
+  sig_y = tup[1]
+  theta = tup[2]
+  mean = tup[3]
   a = np.cos(theta)**2/(2*sig_x**2)+np.sin(theta)**2/(2*sig_y**2)
   b = -np.sin(2*theta)/(4*sig_x**2)+np.sin(2*theta)/(4*sig_y**2)
   c = np.sin(theta)**2/(2*sig_x**2)+np.cos(theta)**2/(2*sig_y**2)
@@ -40,7 +45,7 @@ class Synthetics(MultiOptions):
     self.opdict['outdir'] = os.path.join('../results',self.opdict['dir'])
     self.opdict['fig_path'] = '%s/figures'%self.opdict['outdir']
 
-    self.sep = 'very_well'
+    self.sep = 'bad'
     self.opdict['feat_train'] = '%s_%dc_train.csv'%(self.sep,len(self.opdict['types']))
     self.opdict['feat_test'] = '%s_%dc_test.csv'%(self.sep,len(self.opdict['types']))
     self.opdict['label_train'] = '%s_%dc_train.csv'%(self.sep,len(self.opdict['types']))
@@ -71,24 +76,27 @@ class Synthetics(MultiOptions):
     self.NB_class = len(self.opdict['types'])
     NB_test_all = 1200
     NB_train_all = int(0.4*NB_test_all)
-    if self.NB_class == 2:
-      prop = (.5,.5)
-    elif self.NB_class == 3:
-      prop = (1./3,1./3,1./3)
-      #prop = (0.25,0.5,0.25)
 
-    if len(prop) != self.NB_class:
+    # Choose the proportions in the test set
+    if self.NB_class == 2:
+      prop_test = (.5,.5)
+
+    elif self.NB_class == 3:
+      prop_test = (1./3,1./3,1./3)
+      prop_test = (0.3,0.1,0.6)
+
+    # Choose the proportions in the training set
+    prop_train = prop_test
+    prop_train = (.25,.75)
+
+    if len(prop_test) != self.NB_class:
       print "Warning ! Check number of classes and proportions"
       sys.exit()
-    #if (1-np.sum(prop)) < 10**-6:
-    #  print "Warning ! Set correct proportions of the classes"
-    #  sys.exit()
-
 
     self.NB_train, self.NB_test = {},{}
     for i in range(self.NB_class):
-      self.NB_train[i] = int(NB_train_all*prop[i])
-      self.NB_test[i] = int(NB_test_all*prop[i])
+      self.NB_train[i] = int(NB_train_all*prop_train[i])
+      self.NB_test[i] = int(NB_test_all*prop_test[i])
     print "Training set:", self.NB_train
     print "Test set:", self.NB_test
 
@@ -107,42 +115,43 @@ class Synthetics(MultiOptions):
     NB_test_all = np.sum(self.NB_test.values())
 
     from math import pi
-    # Basis
-    b_sig_x = 4
-    b_sig_y = 2
-    b_theta = -pi/6
-    b_mean = [0,0]
+    ### Choose the repartition of the features values ###
+    ### May be different for the test and training sets...
 
+    ### CLASS 0 ###
+    class_0_test = (4, 2, -pi/6, [0,0])
+    class_0_train = class_0_test
+    #class_0_train = (2, 2, -pi/6, [0.1,0.1])
+
+    ### CLASS 1 ###
     if self.sep == 'well':
       # Well separated
-      sig_x = 2
-      sig_y = 7
-      theta = 0
-      mean = [.85,-.5]
+      class_1_test = (2, 7, 0, [.85,-.5])
+      class_1_train = class_1_test
+      #class_1_train = (3, 4, 0, [.70,-.3])
+
     elif self.sep == 'very_well':
-      sig_x = 4
-      sig_y = 3
-      theta = pi/4
-      mean = [1.5,.9]
+      class_1_test = (4, 3, pi/4, [1.5,.9])
+      class_1_train = class_1_test
+
     elif self.sep == 'bad':
       # Badly separated
-      sig_x = 4
-      sig_y = 2
-      theta = 0
-      mean = [0.25,-.25]
+      class_1_test = (4, 2, 0, [0.25,-.25])
+      class_1_train = class_1_test
 
+    ### CLASS 3 ###
     if self.NB_class == 3:
-      sig_x_3c = 4
-      sig_y_3c = 3
-      theta_3c = pi/4
-      mean_3c = [.5,-.1]
+      class_2_test = (4, 3, pi/4, [.5,-.1])
+      class_2_train = class_2_test
 
+
+    ### Fill the training set ###
     s = {}
-    s[0] = create_synthetics(self.NB_train[0],b_sig_x,b_sig_y,b_theta,b_mean)
-    s[1] = create_synthetics(self.NB_train[1],sig_x,sig_y,theta,mean)
+    s[0] = create_synthetics(self.NB_train[0],class_0_train)
+    s[1] = create_synthetics(self.NB_train[1],class_1_train)
     s_val = np.concatenate([s[0],s[1]])
     if self.NB_class == 3:
-      s[2] = create_synthetics(self.NB_train[2],sig_x_3c,sig_y_3c,theta_3c,mean_3c)
+      s[2] = create_synthetics(self.NB_train[2],class_2_train)
       s_val = np.concatenate([s_val,s[2]])
 
     x_train = {}
@@ -151,12 +160,13 @@ class Synthetics(MultiOptions):
     x_train = pd.DataFrame(x_train)
     x_train.index = [(i,'STA','Z') for i in range(len(x_train))]
 
+    ### Fill the test set ###
     s = {}
-    s[0] = create_synthetics(self.NB_test[0],b_sig_x,b_sig_y,b_theta,b_mean)
-    s[1] = create_synthetics(self.NB_test[1],sig_x,sig_y,theta,mean)
+    s[0] = create_synthetics(self.NB_test[0],class_0_test)
+    s[1] = create_synthetics(self.NB_test[1],class_1_test)
     s_val = np.concatenate([s[0],s[1]])
     if self.NB_class == 3:
-      s[2] = create_synthetics(self.NB_test[2],sig_x_3c,sig_y_3c,theta_3c,mean_3c)
+      s[2] = create_synthetics(self.NB_test[2],class_2_test)
       s_val = np.concatenate([s_val,s[2]])
 
     x_test = {}
@@ -165,6 +175,7 @@ class Synthetics(MultiOptions):
     x_test = pd.DataFrame(x_test)
     x_test.index = [(i,'STA','Z') for i in range(len(x_test))]
 
+    ### Fill the labels ###
     types = ['A','B','C']
     y_train = pd.DataFrame(index=range(NB_train_all),columns=['Type','Date','TypeNum'],dtype=str)
     lim = 0
@@ -237,6 +248,8 @@ class Synthetics(MultiOptions):
     plt.hist(y_hist,bins=bins_2,color=colors_h,normed=1,histtype='stepfilled',alpha=.5)
     for key in sorted(g_y):
       plt.plot(bins_2,g_y[key],color=colors_g[key],lw=2.,label='Class %s'%self.opdict['types'][key])
+    plt.plot([.5,.5],[0,2],'g--',lw=2.)
+    plt.figtext(.52,.7,'?',color='g',size=20)
     plt.xlabel(feat_2)
     plt.legend(loc=2)
     plt.savefig('%s/histo_%s.png'%(self.opdict['fig_path'],feat_2))
@@ -255,7 +268,7 @@ def plot_sep(opt):
     rate_svm = opt.success
     t_svm = opt.threshold
 
-    opt.plot_PDFs()
+    #opt.plot_PDFs()
 
     ### LOGISTIC REGRESSION ###
     opt.opdict['method'] = 'lr'
@@ -278,25 +291,25 @@ def plot_sep(opt):
 
     # PLOTS
     plot_2f_synthetics(theta_svm,rate_svm,t_svm,'SVM',x_train,x_test,y_test,y_train=y_train)
-    plt.savefig('%s/Test_%dc_%s_SVM_ineq.png'%(len(opt.types),opt.opdict['fig_path'],opt.sep))
+    plt.savefig('%s/Test_%dc_%s_SVM_ineq.png'%(opt.opdict['fig_path'],len(opt.types),opt.sep))
     plt.show()
 
     if len(theta_lr) == 1:
       plot_2f_synthetics(theta_lr[0],rate_lr[0],t_lr[0],'LR',x_train,x_test,y_test,y_train=y_train)
-      plt.savefig('%s/Test_%dc_%s_LR_ineq.png'%(len(opt.types),opt.opdict['fig_path'],opt.sep))
+      plt.savefig('%s/Test_%dc_%s_LR_ineq.png'%(opt.opdict['fig_path'],len(opt.types),opt.sep))
       plt.show()
       plot_2f_synthetics(theta_lr[0],rate_lr[0],t_lr[0],'LR',x_train,x_test,y_test,th_comp=theta_svm,p=rate_svm,t_comp=t_svm,y_train=y_train)
-      plt.savefig('%s/Test_3c_%s_ineq.png'%(len(opt.types),opt.opdict['fig_path'],opt.sep))
+      plt.savefig('%s/Test_%dc_%s_ineq.png'%(opt.opdict['fig_path'],len(opt.types),opt.sep))
       plt.show()
 
     elif len(theta_lr) > 1:
       plot_2f_variability(theta_lr,rate_lr,t_lr,'LR',x_train,x_test,y_test,opt.NB_test)
-      plt.savefig('%s/Test_%dc_LR_%s.png'%(len(opt.types),opt.opdict['fig_path'],opt.sep))
+      plt.savefig('%s/Test_%dc_LR_%s.png'%(opt.opdict['fig_path'],len(opt.types),opt.sep))
       plt.show()
 
     if opt.opdict['plot_prec_rec']:
       plot_2f_synth_var(theta_lr,rate_lr,t_lr,'LR',x_train,x_test,y_test,opt.NB_test)
-      plt.savefig('%s/Test_%dc_bad_ineq_threshold.png'%(len(opt.types),opt.opdict['fig_path']))
+      plt.savefig('%s/Test_%dc_bad_ineq_threshold.png'%(opt.opdict['fig_path'],len(opt.types)))
       plt.show()
 
 
